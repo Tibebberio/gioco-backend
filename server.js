3d7439132f378d85d254991659188edfed4aa9a8
Tibebberio <139078551+Tibebberio@users.noreply.github.com>
2025-05-13 22:24:47 +0200

    Update server.js

--- server.js
+++ server.js
@@ -1,15 +1,22 @@
-
 const express = require("express");
 const cors = require("cors");
 const fs = require("fs");
+const path = require("path");
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.use(cors());
 app.use(express.json());
 
+// Servire file statici dalla cartella 'public'
+app.use(express.static(path.join(__dirname, "public")));
+
 const dbFile = "./players.json";
 if (!fs.existsSync(dbFile)) fs.writeFileSync(dbFile, "{}");
+
+app.get("/", (req, res) => {
+  res.send("Server attivo!");
+});
 
 app.get("/profilo/:playerName", (req, res) => {
   const db = JSON.parse(fs.readFileSync(dbFile));
